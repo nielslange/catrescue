ecbaebbb276c98f133a220d833c42da09c2d4185
Niels Lange <info@nielslange.de>
2025-03-09 13:36:17 +0700

    Simplify home-location.php

--- template-parts/home-location.php
+++ template-parts/home-location.php
@@ -7,38 +7,35 @@
  * @package Catrescue
  */
 
-class_exists( 'ACF' ) || exit( 'ACF is required.' );
-
-$location_image_id     = get_post_meta( get_the_ID(), 'location_image', true );
-$location_image        = wp_get_attachment_image_src( $location_image_id, 'full' );
-$location_headline     = get_post_meta( get_the_ID(), 'location_headline', true );
-$location_teaser_count = get_post_meta( get_the_ID(), 'location_teaser', true );
-$location_teaser       = array();
-
-if ( $location_teaser_count ) {
-	for ( $i = 0; $i < $location_teaser_count; $i++ ) {
-		$location_teaser[] = array(
-			'headline' => get_post_meta( get_the_ID(), 'location_teaser_' . $i . '_location_teaser_subline', true ),
-			'content'  => get_post_meta( get_the_ID(), 'location_teaser_' . $i . '_location_teaser_content', true ),
-		);
-	}
+if ( ! function_exists( 'get_field' ) ) {
+	exit( 'ACF is required.' );
 }
+
+$location_image        = get_field( 'location_image' );
+$location_headline     = get_field( 'location_headline' );
+$location_teaser_count = get_field( 'location_teaser' );
 
 ?>
 
-<div id="location" class="hero" style="background-image: url(<?php print( esc_html( $location_image[0] ) ); ?>)">
+<div id="location" class="hero" style="background-image: url(<?php print( esc_html( $location_image['url'] ) ); ?>)">
 	<div class="overlay"></div>
 	<div class="content">
 		<h2><?php print( esc_html( $location_headline ) ); ?></h2>
 		<div class="teaser">
 		<?php
-		foreach ( $location_teaser as $teaser ) {
-			printf(
-				'<div><h3>%s</h3>%s</div>',
-				esc_html( $teaser['headline'] ),
-				wp_kses_post( $teaser['content'] )
-			);
-		}
+		if ( have_rows( 'location_teaser' ) ) :
+			while ( have_rows( 'location_teaser' ) ) :
+				the_row();
+				$subline = get_sub_field( 'location_teaser_subline' );
+				$content = get_sub_field( 'location_teaser_content' );
+
+				printf(
+					'<div><h3>%s</h3>%s</div>',
+					esc_html( $subline ),
+					wp_kses_post( $content )
+				);
+			endwhile;
+		endif;
 		?>
 		</div>
 	</div>
